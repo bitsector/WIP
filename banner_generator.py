25a5a97d3717f609c9486582a7815e4c1e61a310
bitsector <61909827+bitsector@users.noreply.github.com>
2023-09-30 16:43:02 +0300

    Text proportion and placement very good, no bg, only gray

--- banner_generator.py
+++ banner_generator.py
@@ -1,52 +1,32 @@
 #!/usr/bin/python3
 
 from PIL import Image, ImageDraw, ImageFont
-from collections import namedtuple
 
-def create_banner_with_transparent_bg(bg_path, profile):
-    # Load the background image and make it half transparent
-    bg_image = Image.open(bg_path).convert("RGBA")
-    for x in range(bg_image.width):
-        for y in range(bg_image.height):
-            r, g, b, a = bg_image.getpixel((x, y))
-            bg_image.putpixel((x, y), (r, g, b, 128))  # Setting alpha to 128 for half transparency
-
-    banner = bg_image
+def create_banner(bg_color, text):
+    # Create a plain grey background with double the LinkedIn recommended resolution
+    banner = Image.new('RGB', (3168, 792), bg_color)
     
     # Draw text on the banner
     draw = ImageDraw.Draw(banner)
     font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"  # Path to DejaVuSans-Bold font on Ubuntu
-    font_size = 25
+    font_size = 50
     font = ImageFont.truetype(font_path, font_size)
     
-    y_position = 10
+    y_position = 20  # Starting from the top
+    x_position = 20  # Start from the upper left
 
-    # Modified list of attributes to maintain consistent line spacing
-    attributes = [
-        ("i_am", profile.i_am),
-        ("use", profile.use),
-        ("cloud", profile.cloud),
-        ("specialize", profile.specialize),
-        ("edu", profile.edu)
-    ]
+    # Split the text by lines and draw each line
+    for line in text.splitlines():
+        draw.text((x_position, y_position), line, font=font, fill='white')
+        y_position += 60  # Adjust spacing between lines
 
-    for name, value in attributes:
-        if isinstance(value, list):
-            draw.text((10, y_position), f"{name}=" + repr(value) + ",", font=font, fill='white')
-        else:
-            draw.text((10, y_position), value, font=font, fill='white')
-        y_position += 30
+    # Resize the image to fit LinkedIn's recommended size
+    banner = banner.resize((1584, 396))
 
     # Save the banner
     banner.save("my_banner.png")
 
 if __name__ == "__main__":
-    Profile = namedtuple('Profile', ['i_am', 'use', 'cloud', 'specialize', 'edu'])
-    user_profile = Profile(
-        i_am=["software engineer", "tech enthusiast", "make cloud stuff connect", "like to get my hands dirty"],
-        use=["Kubernetes", "Go/Python/C++", "Linux", "Docker", "DevOps"],
-        cloud=["AWS", "GCP", "Azure"],
-        specialize=["Automation", "Micro-services", "REST-API"],
-        edu=["B.Sc Mathematics and computer science", "M.Sc Computer Science - ONGOING"]
-    )
-    create_banner_with_transparent_bg("bg.png", user_profile)
+    with open("desired_text.txt", "r") as file:
+        text = file.read()
+    create_banner("#B0B0B0", text)  # #B0B0B0 is a shade of grey similar to the background of the provided image
